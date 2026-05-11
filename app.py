c46824bed6a59469d3b6698c84c278ddc0ed0369
twelof <odhiambok936@gmail.com>
2026-05-11 18:41:03 +0300

    clean ap

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI`')
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY']=os.getenv('SECRET_KEY')
